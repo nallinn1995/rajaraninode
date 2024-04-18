8fceeed75d3d0ea7ab39705c96aa19072ebee538
Tharun <tharun24062001@gmail.com>
2024-04-18 14:33:19 +0530

    changes updated

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const io = require("socket.io")(httpServer, {
 const rooms = {};
 
 io.on("connection", (socket) => {
-  console.log("User connected");
- 
+  
+
   let connectedPlayers = [];
   function broadcastPlayerList() {
     io.emit("playerList", connectedPlayers);
@@ -20,36 +20,34 @@ io.on("connection", (socket) => {
     socket.emit("roomCreated", roomId);
   });
 
-  socket.on('getPlayers', (roomId) => {
-    console.log(`Received request for players in room: ${roomId}`);
+  socket.on("getPlayers", (roomId) => {
+    
 
     // Check if the room exists in the rooms object
     if (rooms[roomId] && rooms[roomId].players) {
-        // Retrieve the list of players in the room
-        const playersInRoom = rooms[roomId].players;
+      // Retrieve the list of players in the room
+      const playersInRoom = rooms[roomId].players;
 
-        // Emit the list of players back to the client who requested it
-        socket.emit('playerList', playersInRoom);
+      // Emit the list of players back to the client who requested it
+      socket.emit("playerList", playersInRoom);
 
-        console.log(`Sent player list to client ${socket.id}:`, playersInRoom);
+      
     } else {
-        console.log(`Room ${roomId} does not exist or has no players`);
-        socket.emit('playerList', []);
+      
+      socket.emit("playerList", []);
     }
-});
+  });
 
-socket.on("playUpdate",(playerObj) => {
-    console.log(playerObj)
-    for(let data of rooms[playerObj.roomId].players){
-        if(playerObj.playerName == data.name){
-            data["play"] = true
-        }
+  socket.on("playUpdate", (playerObj) => {
+    
+    for (let data of rooms[playerObj.roomId].players) {
+      if (playerObj.playerName == data.name) {
+        data["play"] = true;
+      }
     }
-})
+  });
 
-
-
-  socket.on("joinroom", ({ playerName, roomId, avatar, host }) => {
+  socket.on("joinroom", ({ playerName, roomId, avatar, host, playerId }) => {
     // Make sure the socket joins the room
     socket.join(roomId);
 
@@ -58,35 +56,48 @@ socket.on("playUpdate",(playerObj) => {
 
     // Check if the room already contains 4 players
     if (rooms[roomId].players.length < 4) {
-        // Add the player to the room
-        rooms[roomId].players.push({ roomId: roomId, name: playerName, avatar: avatar, host: host });
-        
-        // Broadcast to the room that a player has joined
-        socket.to(roomId).emit("playerJoined", playerName);
+      // Add the player to the room
+      rooms[roomId].players.push({
+        roomId: roomId,
+        name: playerName,
+        avatar: avatar,
+        host: host,
+        playerId: playerId,
+      });
 
-        // Send the room ID to the player who joined
-        socket.emit("roomid", roomId);
-        socket.emit("roomMem", "Waiting for players to join");
-        
-        // Notify all players in the room about the updated list of players
-        const uniquePlayerNames = new Set(
-            rooms[roomId].players.map((player) => player)
-        );
-        io.to(roomId).emit(
-            "playersList",
-            Array.from(uniquePlayerNames)
-        );
+      // Broadcast to the room that a player has joined
+      socket.to(roomId).emit("playerJoined", playerName);
+
+      // Send the room ID to the player who joined
+      socket.emit("roomid", roomId);
+      socket.emit("roomMem", "Waiting for players to join");
+
+      // Notify all players in the room about the updated list of players
+      const uniquePlayerNames = new Set(
+        rooms[roomId].players.map((player) => player)
+      );
+      io.to(roomId).emit("playersList", Array.from(uniquePlayerNames));
     } else {
-        console.log("vvvvvvvvvvvvvvvvvv")
-        // If the room already contains 4 players, notify the player that the room is full
-        socket.emit("roomFull", true);
+      
+      // If the room already contains 4 players, notify the player that the room is full
+      socket.emit("roomFull", true);
     }
-});
+  });
 
+  socket.on("selectCard", (cardObj, playerId, roomid) => {
+    for (let data of rooms[roomid].players) {
+      if (data.playerId == playerId) {
+        data["selected"] = true;
+        data["role"] = cardObj?.role;
+      }
+    }
+    console.log(rooms[roomid]);
+    socket.emit("updatedPlayerListCard", rooms[roomid].players);
+  });
 
 
 });
 
 httpServer.listen(3500, () => {
-  console.log("Listening at 3500");
+  
 });
